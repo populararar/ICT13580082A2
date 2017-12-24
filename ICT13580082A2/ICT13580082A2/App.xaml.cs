20f5d7e5dbd46dfa59881c5f722251ec2cc60257
popular_ar <popular23p@gmail.com>
2017-12-24 17:48:13 +0700

    init

--- ICT13580082A2/ICT13580082A2/App.xaml.cs
+++ ICT13580082A2/ICT13580082A2/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ICT13580082A2.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,17 @@ namespace ICT13580082A2
 {
     public partial class App : Application
     {
+        public static DbHelper DbHelper { get; set; }
         public App()
         {
             InitializeComponent();
+        }
+        public App(string dbPath)
+        {
+            InitializeComponent();
 
-            MainPage = new ICT13580082A2.MainPage();
+            DbHelper = new DbHelper(dbPath);
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
